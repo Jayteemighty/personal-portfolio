0d89ae72c8b707486f3e94af458173600959c3a0
Adesina Joshua <tolujosh1@gmail.com>
2024-09-14 18:30:52 +0100

    completed picture and intro side of portfo,lio

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,12 +7,11 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Ada</h1>
+        <h1 className={styles.title}>Hi, I'm Joshua</h1>
         <p className={styles.description}>
-          I'm a full-stack developer with 5 years of experience using React and
-          NodeJS. Reach out if you'd like to learn more!
+          I'm a Software Engineer with 2 years of experience using Django and FastAPI. Reach out if you'd like to learn more!
         </p>
-        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+        <a href="mailto:tolujosh1@gmail.com" className={styles.contactBtn}>
           Contact Me
         </a>
       </div>
